c38683f4e16a2fd295abc7104de6bb933a3d7998
Marcelo Klinger <marcelo.klinger@hotmail.com>
2021-03-11 16:01:46 -0300

    Array and forEach

--- Formulario_Tabela/index.php
+++ Formulario_Tabela/index.php
@@ -2,9 +2,9 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $name = (string)$_POST['fullname'];
   $email = (string)$_POST['email'];
-  $subject = $_POST['subject'];
-  $items_list = $_POST['items_list'];
-  $gender = $_POST['gender'];
+  $subject = (string)$_POST['subject'];
+  $items_list = (string)$_POST['items_list'];
+  $gender = (string)$_POST['gender'];
   $message = (string)$_POST['message'];
 
   echo 'Nome cadastrado: ' . $name;
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <body class="min-h-screen flex items-center justify-center bg-blue-400">
   <div class="bg-white p-16 rounded shadow-2x1 w-2/3">
-    <h2 class="text-3x1 font-bold mb-8">Formulário de contato</h2>
+    <h2 class="text-center text-2xl text-gray-800 pb-8">Formulário de contato</h2>
     <form action="index.php" method="post" class="space-y-3">
       <div class="flex flex-wrap items-stretch w-full mb-4 relative">
         <label class="mb-1 font-bold text-sm pr-4" name="fullname">Nome Completo: </label>
@@ -42,11 +42,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <input class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 px-2 border-gray-400 border border-gray-400 rounded-lg outline-none focus:border-blue-400" type="text" name="email" required="required">
       </div>
       <div class="flex flex-wrap items-stretch w-full mb-4 relative">
-        <label class="mb-1 font-bold text-sm pr-4" nome="subject">Assunto</label>
+        <label class="mb-1 font-bold text-sm pr-4" nome="subject">Assunto: </label>
         <input class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 px-2 border-gray-400 border border-gray-400 rounded-lg outline-none focus:border-blue-400" type="text" name="subject" required="required">
       </div>
       <div class="flex flex-wrap items-stretch w-full mb-4 relative">
-        <label class="mb-1 font-bold text-sm pr-4" nome="items_list">Opções</label>
+        <label class="mb-1 font-bold text-sm pr-4" nome="items_list">Opções: </label>
         <select required class="px-2 border focus:border-blue-400" id="items_list" name="items_list">
           <option style="display:none;"></option>
           <option value="item_list1">Item 1</option>
@@ -56,18 +56,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </select>
       </div>
       <div>
-        <label class="block mb-1 font-bold text-sm" for="gender">Sexo</label>
+        <label class="block mb-1 font-bold text-sm" for="gender">Sexo: </label>
         <input class="border border-gray-400 p-1 rounded-lg outline-none focus:border-blue-400" type="radio" required id="male" name="gender" value="masculino">
         <label class="mb-1 font-bold text-sm" for="male">Masculino</label><br>
         <input class="border border-gray-400 p-1 rounded-lg outline-none focus:border-blue-400" type="radio" id="female" name="gender" value="feminino">
         <label class="mb-1 font-bold text-sm" for="female">Feminino</label><br>
       </div>
       <div>
-        <label class="block mb-1 font-bold text-sm" for="mensage">Mensagem</label>
+        <label class="block mb-1 font-bold text-sm" for="mensage">Mensagem: </label>
         <textarea class="h-40 w-full border border-gray-400 p-1 rounded-lg outline-none focus:border-blue-400" required name="message">
       </textarea>
       </div>
-      <button class="block w-full bg-green-400 p-4 rounded" type="submit" value="enviar">ENVIAR</button>
+      <button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" type="submit" value="enviar">ENVIAR</button>
     </form>
   </div>
 </body>
